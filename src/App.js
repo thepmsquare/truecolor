6799828d6cc7e9649c8603174ab2aefc948f75c9
thePmSquare <impmmmm@gmail.com>
2020-09-06 21:08:52 +0530

    implement game

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import Game from "./Game";
 import "./stylesheets/App.css";
 
 class App extends Component {
+  changeBackground = (hex) => {
+    document.querySelector(".App").style.backgroundColor = hex;
+  };
   render = () => {
     return (
       <div className="App">
-        <Game />
+        <Game changeBackground={this.changeBackground} />
       </div>
     );
   };
